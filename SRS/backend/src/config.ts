15b86197ae2d5ad29752cda1c0c8429bbd7a1bf2
lesedi <analystmodikwe18@gmail.com>
2026-09-16 13:32:01 +0200

    fixing the config

--- SRS/backend/src/config.ts
+++ SRS/backend/src/config.ts
@@ -1,18 +1,17 @@
 // seat reservation rules to hold  seats, expiry time, Maximum concurrent holds per user, Maximum holds per user per hour,Maximum extensions per hold
 
 export const RESERVE_CONFIG = {
-    SEATS_PER_EVENT : 20,
+    SEATS_PER_EVENT :Number(process.env.SEATS_PER_EVENT ?? 20) ,
 
     HOLD_CODE_LENGTH : 6,
 
-    HOLD_EXPIRY_TIME: Number(process.env.HOLD_EXPIRY_TIME_SECONDS ?? 60),
+    HOLD_CODE_CHARACTERS: "ABCDEFGHJKMNPQRSTUVWXYZ23456789",
 
-    MAXIMUM_CONCURRENT_HOLDS_PER_USER: 2,
+    HOLD_EXPIRY_TIME_SECONDS: Number(process.env.HOLD_EXPIRY_TIME_SECONDS ?? 60),
 
-    MAXIMUM_HOLDS_PER_USER_PER_HOUR: (process.env.MAXIMUM_HOLDS_PER_USER_PER_HOUR ?? 5),
+    MAXIMUM_CONCURRENT_HOLDS_PER_USER: Number(process.env.MAXIMUM_CONCURRENT_HOLDS_PER_USER ?? 2),
 
-    MAXIMUM_EXTENSIONS_PER_HOLD: 2
+    MAXIMUM_HOLDS_PER_USER_PER_HOUR: Number (process.env.MAXIMUM_HOLDS_PER_USER_PER_HOUR ?? 5),
 
-
-
-}
+    MAXIMUM_EXTENSIONS_PER_HOLD: Number(process.env.MAXIMUM_EXTENSIONS_PER_HOLD ?? 2),
+};

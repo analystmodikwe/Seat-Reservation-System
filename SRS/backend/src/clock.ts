1a736fb545560e89e587a6fb24fe726891be9777
lesedi <analystmodikwe18@gmail.com>
2026-09-16 20:45:40 +0200

    adding a clock rule to avoiding using date.now()

---
+++ SRS/backend/src/clock.ts
@@ -0,0 +1,12 @@
+// creating a small abstraction around time so the my services dont directly use Date.now()
+// when the app ask for clock it gets the system time
+
+export interface Clock {
+    now(): number;
+}
+
+export class SystemClock implements Clock {
+    now(): number {
+        return Date.now();
+    }
+}

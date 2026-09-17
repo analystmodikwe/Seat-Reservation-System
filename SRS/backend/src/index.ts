7141868391ead3c5b62e92b01bc302ead5b667b8
lesedi <analystmodikwe18@gmail.com>
2026-09-17 21:46:28 +0200

    adding into index

--- SRS/backend/src/index.ts
+++ SRS/backend/src/index.ts
@@ -1,7 +1,11 @@
-// index.ts
+
 import app from "./app";
+import { expiryScheduler } from "./container";
 
 const PORT = process.env.PORT ?? 5000;
+
+expiryScheduler.start();
+
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
 });

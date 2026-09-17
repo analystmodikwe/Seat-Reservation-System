d15a5c20c6a1ecdc68c6d774933b35d592b44712
lesedi <analystmodikwe18@gmail.com>
2026-09-17 14:06:20 +0200

    adding domain error and hold service

--- SRS/backend/src/holdService.ts
+++ SRS/backend/src/holdService.ts
@@ -7,3 +7,22 @@ import { HoldRepository } from "./repositories/holdRepository";
 import { SeatRepository } from "./repositories/seatRepository";
 import { EventLogRepository } from "./repositories/eventLogRepository";
 import { Hold, HoldResponse, PlaceHoldRequest } from "./types";
+
+// this will be thrown when a request breaks one of the reservation rules
+// the "rule" will give our API a meaningful error that names which rule was violated
+export class DomainError extends Error {
+    constructor(public rule: string, message: string) {
+        super(message);
+    }
+}
+
+export class HoldService {
+    constructor(
+        private seatRepository: SeatRepository,
+        private holdRepository: HoldRepository,
+        private eventLogRepository: EventLogRepository,
+        private codeGenerator: CodeGenerator,
+        private lock: Lock,
+        private clock: Clock
+    ) {}
+}

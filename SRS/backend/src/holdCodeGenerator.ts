a6aabcb2baa17dec41371c76254d88af69f66b1d
lesedi <analystmodikwe18@gmail.com>
2026-09-17 03:43:31 +0200

    fixing and adding hold code generator

--- SRS/backend/src/holdCodeGenerator.ts
+++ SRS/backend/src/holdCodeGenerator.ts
@@ -1,14 +1,28 @@
 import {  RESERVE_CONFIG } from "./config";
+import{ HoldRepository } from "./repositories/holdRepository";
 
 
 // here i am going to generate a 6 char code from the RESERVE_CONFIG.HOLD_CODE_CHARACTERS But it must follow the rules of RESERVE_CONFIG.HOLD_CODE_CHARACTERS it shouldnt contain any similar looking characters like 0 and O, 1 and I, L and 1, etc.
 
 export interface CodeGenerator{
- generateCode() : string;
+    generateCode() : string;
 }
 
-export class holdCodeGenerator implements CodeGenerator {
+export class HoldCodeGenerator implements CodeGenerator {
+    constructor(private holdRepository: HoldRepository) {}
+
+
     generateCode(): string {
+        let code: string;
+        do {
+            code = this.randomCode();
+        } while (this.holdRepository.findByCode(code) !== undefined)
+        return code;
+    }
+
+
+
+    private randomCode(): string {
         let code = "";
 
         for (let i = 0; i < RESERVE_CONFIG.HOLD_CODE_LENGTH; i++) {
@@ -21,3 +35,4 @@ export class holdCodeGenerator implements CodeGenerator {
         return code;
     }
 }
+
